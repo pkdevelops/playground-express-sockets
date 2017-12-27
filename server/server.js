6211f90cfb8c600923342789331c24207afd9b24
Pradeep <pradeepdevelops@gmail.com>
2017-12-27 16:16:25 -0600

    Init commit

---
+++ server/server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const path = require('path')
+
+const PORT = process.env.PORT || 3000
+const app = express()
+const publicPath = path.join(__dirname, '../public')
+app.use(express.static(publicPath))
+
+app.listen(PORT, () => console.log(`Server is up on port ${PORT}.`))
